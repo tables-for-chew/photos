61d4932c3473cb0aa4fe6c894c03689bc2191c2e
Steve Duong <steve@Plan.D>
2019-03-04 23:50:20 -0800

    adds changes to seed for 10M

--- database/newseed.js
+++ database/newseed.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Determines how many picutres will be set for a given restaurant ( 0  to 20)
 const numberOfPictures = () => {
-  return Math.floor(Math.random() * 21)
+  return Math.floor(Math.random() * 11)
 }
 
 // global variables
@@ -17,35 +17,35 @@ var imageCollection = '';
 let imageID = 1;
 
 var batcher = () => {
-  if (iteration === 1) {
+  if (iteration === 5000) {
     return
   };
   iteration++
 
   // creates 1000 unique restaurants
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 2000; i++) {
     var restaurant = [];
-    restaurant.push(`id: ${restaurantID}`);
-    restaurant.push(`restaurant_name: ${faker.company.catchPhraseNoun()}`);
+    restaurant.push(`${restaurantID}`);
+    restaurant.push(`${faker.company.catchPhraseNoun()}`);
 
-    restaurantCollection += JSON.stringify(restaurant) + '\n';
+    restaurantCollection += JSON.stringify(restaurant).slice(1, -1) + '\n';
     // creates data for each indiviual picture
     let pictures = numberOfPictures()
     for (let j = 1; j <= pictures; j++) {
       var image = [];
-      image.push(`id: ${imageID}`);
-      image.push(`restaurant_id: ${restaurantID}`);
-      image.push(`username: ${faker.name.firstName()}`);
-      image.push(`date_posted: ${faker.date.recent()}`);
-      image.push(`image_url: https://s3.us-east-2.amazonaws.com/photochews/food${k}.jpg`);
-      image.push(`caption: ${faker.lorem.words()}`);
-      image.push(`hover_data: ${faker.lorem.words()}`);
+      image.push(`${imageID}`);
+      image.push(`${restaurantID}`);
+      image.push(`https://s3.us-east-2.amazonaws.com/photochews/food${k}.jpg`);//url
+      image.push(`${faker.lorem.words()}`); //caption
+      image.push(`${faker.date.past().toDateString()}`); //date post
+      image.push(`${faker.name.firstName()}`); //username
+      image.push(`${faker.lorem.words()}`); // hover data
 
       // only 679 pics uploaded on S3. we will reset count after we reach 679
       k++
       if (k === 680) { k = 1 };
 
-      imageCollection += JSON.stringify(image) + '\n';
+      imageCollection += JSON.stringify(image).slice(1, -1) + '\n';
       imageID += 1;
     }
     restaurantID += 1
