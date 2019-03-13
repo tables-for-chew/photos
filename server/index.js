9feb8e413df564e86b27ce314307ee2bd300ab32
Steve Duong <steve@Plan.D>
2019-03-13 15:51:01 -0700

    bug fix

--- server/index.js
+++ server/index.js
@@ -18,14 +18,5 @@ app.post('/api/create/photos/:id', controller.postPhoto);
 app.put('/api/update/photos/:id/:photoName', controller.updatePhoto);
 app.delete('/api/delete/photos/:id/:photoName', controller.deletePhoto);
 
-app.get('/loaderio-213df0b7319eb3d7d03a853733cab103/', (req, res) => {
-  res.sendFile('loaderio.txt', function (err) {
-    if (err) {
-      next(err);
-    } else {
-      console.log('Sent:', fileName);
-    }
-  })
-})
 app.use('/:id', express.static(path.join(__dirname, '../client/dist')));
 app.listen(PORT, console.log(`Listening to PORT ${PORT}...`));
